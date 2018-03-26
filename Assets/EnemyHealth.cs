542ccf5c0a9ec6c5fbd73721ee998700a8eb8db2
Oprea Alexandru <oalexprea@yahoo.com>
2018-03-27 00:17:46 +0300

    Enemies are dieing now

---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public FloatReference maxHp;
+
+    private float currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHp.CurrentValue;
+    }
+    
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+
+        if (currentHealth < 0f)
+            Die();
+    }
+
+    private void Die()
+    {
+        Destroy(transform.parent.gameObject);
+    }
+
+
+
+
+}
--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float damage;
     public float speed;
     private Rigidbody2D rb;
 
@@ -16,6 +17,15 @@ public class Bullet : MonoBehaviour
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        EnemyHealth enemy = collision.gameObject.GetComponent<EnemyHealth>();
+        if(enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
     }
 
 }
