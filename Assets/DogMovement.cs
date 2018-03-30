7473753646f62d681ab57068efb88a9441c4dfe0
Oprea Alexandru <oalexprea@yahoo.com>
2018-03-30 15:15:20 +0300

    Dog added with simple follow script + physics layers

---
+++ Assets/DogMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogMovement : Movement
+{
+    public Transform target;
+    private float distanceThreshold = 1f;
+
+    protected override void ReadInput()
+    {
+        // Get the input based on the target location.
+        float horizontalInput = 0f;
+
+        Vector3 targetDirection = (target.position - transform.position);
+        float distance = targetDirection.x;
+        if (Mathf.Abs(distance) > distanceThreshold)
+        {
+            horizontalInput = (distance < 0f) ? (-1f) : (1f);
+        }
+
+        HandleSpriteDirectionWithInput(horizontalInput);
+
+        HandleMovement(horizontalInput);
+    }
+
+
+}
