86c310c53254eebe7669f18771cd170c8c3f377c
Oprea Alexandru <oalexprea@yahoo.com>
2018-04-01 20:20:17 +0300

    Build fixes at UI

---
+++ Assets/WinScreenText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinScreenText : MonoBehaviour
+{
+    public FloatReference flowersCollected;
+
+    private Text text;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        text.text = "I will forgive you because you've collected:" + flowersCollected.CurrentValue + " roses for me. But I am still mad !";
+    }
+
+}
